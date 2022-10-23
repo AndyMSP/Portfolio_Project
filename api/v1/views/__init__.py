b43149dce29842515e7ff8b6114e7359a093e199
Andy <3775@holbertonschool.com>
2022-10-23 17:43:33 -0400

    add import actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ user_views = Blueprint('user_views', __name__, url_prefix='/api/v1')
 action_views = Blueprint('action_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.users import *
+from api.v1.views.actions import *
